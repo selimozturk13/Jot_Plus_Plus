57b336832c0b0392c8a81cc3829c53a7e9063ba6
Selim Öztürk <selimozt2013@gmail.com>
2025-11-12 00:05:51 +0300

    Exporting notes as txt or markdown file feature has added.

--- jotplusplus.cpp
+++ jotplusplus.cpp
@@ -6,9 +6,11 @@
 #include <FL/fl_ask.H>
 #include <FL/Fl_Hold_Browser.H>
 #include <FL/Fl_Menu_Bar.H>
+#include <FL/Fl_Native_File_Chooser.H>
 #include <exception>
 #include <fstream>
 #include "sqlite_modern_cpp/hdr/sqlite_modern_cpp.h"
+#include <ostream>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -117,6 +119,59 @@ void increasesize(Fl_Widget*,void*)
     browser->textsize(browser->textsize()+5);
 }
 
+void exporttxt(Fl_Widget*,void*)
+{
+    Fl_Native_File_Chooser native_chooser;
+    native_chooser.type(Fl_Native_File_Chooser::BROWSE_SAVE_FILE);
+    native_chooser.title("Export Note as TXT");
+    native_chooser.filter("TXT files\t*.txt");
+    native_chooser.preset_file("Notes.txt");
+    switch (native_chooser.show()) {
+        case -1: 
+            cerr << "File chooser ERROR\n";
+            return;
+        case 1: 
+            return;
+    }
+    ofstream notes(native_chooser.filename());
+    try {
+        db << "SELECT * FROM notes;">> [&notes](int id,string title,string content){
+            notes<<"ID: "<<id<<"\n"<<title<<"\n"<<content<<"\n";
+        };
+    }
+    catch(exception &e) {
+        cerr<<"Database error: "<<e.what()<<endl;
+    }
+    fl_message("Note exported succesfully");
+}
+
+void exportmd(Fl_Widget*,void*)
+{
+    Fl_Native_File_Chooser native_chooser;
+    native_chooser.type(Fl_Native_File_Chooser::BROWSE_SAVE_FILE);
+    native_chooser.title("Export Note as MARKDOWN");
+    native_chooser.filter("TXT files\t*.md");
+    native_chooser.preset_file("Notes.md");
+    switch (native_chooser.show()) {
+        case -1: 
+            cerr << "File chooser ERROR\n";
+            return;
+        case 1: 
+            return;
+    }
+    ofstream notes(native_chooser.filename());
+    try {
+        db << "SELECT * FROM notes;">> [&notes](int id,string title,string content){
+            notes<<"ID: *"<<id<<"*\n**"<<title<<"**\n"<<content<<"\n";
+        };
+    }
+    catch(exception &e) {
+        cerr<<"Database error: "<<e.what()<<endl;
+    }
+    fl_message("Note exported succesfully");
+    
+}
+
 int main()
 {
     Fl::scheme("gtk+");
@@ -190,7 +245,8 @@ int main()
 
     Fl_Menu_Bar* menubar=new Fl_Menu_Bar(0,0,width,25);
 
-    
+    menubar->add("File/Export Notes/As a TXT file","",exporttxt,NULL);
+    menubar->add("File/Export Notes/As a MARKDOWN file","",exportmd,NULL);
     menubar->add("View/Dark\\Light mode",FL_CTRL+'l',darklightmode,NULL);
     menubar->add("View/Reduce The Font Size",FL_CTRL+'r',reducesize,NULL);
     menubar->add("View/Increase The Font Size",FL_CTRL+'i',increasesize,NULL);
